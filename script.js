c08d0c3829c1e3d81710e916a467cb7f387f2e4f
tomasraul <tomasceleste22@gmail.com>
2025-09-24 10:45:21 +0200

    script updated

--- script.js
+++ script.js
@@ -45,58 +45,58 @@ botao.addEventListener('click', function(){
 
         let molho = Math.floor(resultado(comp, larg)[2] / 12)
         if(molho >= 1){
-            resposta.innerHTML += `Equivalentes a ${molho} molho(s) `
+            resposta.innerHTML += `Equivalentes a <span class="num">${molho}</span> molho(s) `
         }
         let molho2 = resultado(comp, larg)[2] % 12
         if(molho >= 1 && molho2 > 0){
-            resposta.innerHTML += `e mais ${molho2} barrotes <br>`
+            resposta.innerHTML += `e mais <span class="num">${molho2}</span> barrotes <br>`
         }
 
         if(qr.checked){
             if (comp > 400) {
                 let faltaComprimento = comp - 400
-                resposta.innerHTML += `Comprimento: ${comp/100}m; faltará ${resultado(comp, larg)[1]} pedaços de ${(faltaComprimento/100).toFixed(2)}m cada <br>`
+                resposta.innerHTML += `Comprimento: <span class="num">${comp/100}m</span>; faltará <span class="num">${resultado(comp, larg)[1]}</span> pedaços de <span class="num">${(faltaComprimento/100).toFixed(2)}m</span> cada <br>`
             }
             if (comp < 400) {
                 let restoComprimento = 400 - comp
-                resposta.innerHTML += `Comprimento: ${comp / 100}m; restará ${resultado(comp, larg)[1]} pedaços de ${(restoComprimento/100).toFixed(2)}m cada <br>`
+                resposta.innerHTML += `Comprimento: <span class="num">${comp / 100}m</span>; restará <span class="num">${resultado(comp, larg)[1]}</span> pedaços de <span class="num">${(restoComprimento/100).toFixed(2)}m</span> cada <br>`
             }
             if (larg > 400) {
                 let faltaLargura = larg - 400
-                resposta.innerHTML += `Largura: ${larg / 100}m; faltará ${resultado(comp, larg)[0]} pedaços de ${(faltaLargura/100).toFixed(2)}m cada <br>`
+                resposta.innerHTML += `Largura: <span class="num">${larg / 100}m;</span> faltará <span class="num">${resultado(comp, larg)[0]} </span> pedaços de <span class="num">${(faltaLargura/100).toFixed(2)}m</span> cada <br>`
             }
             if (larg < 400) {
                 let restoLargura = 400 - larg
-                resposta.innerHTML += `Largura: ${larg / 100}m; restará ${resultado(comp, larg)[0]} pedaços de ${(restoLargura/100).toFixed(2)}m cada <br>`
+                resposta.innerHTML += `Largura: <span class="num">${larg / 100}m</span>; restará <span class="num">${resultado(comp, larg)[0]}</span> pedaços de <span class="num">${(restoLargura/100).toFixed(2)}m</span> cada <br>`
             }
         }
 
         if(sx.checked){
             if (comp > 600) {
                 let faltaComprimento = comp - 600
-                resposta.innerHTML += `Comprimento: ${comp / 100}m;  faltará ${resultado(comp, larg)[1]} pedaços de ${(faltaComprimento/100).toFixed(2)}m cada <br>`
+                resposta.innerHTML += `Comprimento: <span class="num">${comp / 100}m</span>;  faltará <span class="num">${resultado(comp, larg)[1]}</span> pedaços de <span class="num">${(faltaComprimento/100).toFixed(2)}m</span> cada <br>`
             }
             if (comp < 600) {
                 let restoComprimento = 600 - comp
-                resposta.innerHTML += `Comprimento: ${comp / 100}m; restará ${resultado(comp, larg)[1]} pedaços de ${(restoComprimento/100).toFixed(2)}m cada <br>`
+                resposta.innerHTML += `Comprimento: <span class="num">${comp / 100}m<span>; restará <span class="num">${resultado(comp, larg)[1]}<span> pedaços de <span class="num">${(restoComprimento/100).toFixed(2)}m</span> cada <br>`
             }
             if (larg > 600) {
                 let faltaLargura = larg - 600
-                resposta.innerHTML += `Largura: ${larg / 100}m; faltará ${resultado(comp, larg)[0]} pedaços de ${(faltaLargura/100).toFixed(2)}m cada <br>`
+                resposta.innerHTML += `Largura: <span class="num">${larg / 100}m<span>; faltará <span class="num">${resultado(comp, larg)[0]}<span> pedaços de <span class="num">${(faltaLargura/100).toFixed(2)}m</span> cada <br>`
             }
             if (larg < 600) {
                 let restoLargura = 600 - larg
-                resposta.innerHTML += `Largura: ${larg / 100}m; restará ${resultado(comp, larg)[0]} pedaços de ${(restoLargura/100).toFixed(2)}m cada <br>`
+                resposta.innerHTML += `Largura: <span class="num">${larg / 100}m</span>; restará <span class="num">${resultado(comp, larg)[0]}</span> pedaços de <span class="num">${(restoLargura/100).toFixed(2)}m</span> cada <br>`
             }
         }
 
 
         let areaChapas = area((comp/100), (larg/100)).toFixed(2)
         let numChapas = Math.ceil(areaChapas / 3.6)
-        chapas.innerHTML += `-Area: ${areaChapas}m<sup>2</sup>; <br> -Chapas Necessarias ${numChapas}, `
+        chapas.innerHTML += `-Area: <span class="num">${areaChapas}m<sup>2</sup></span>; <br> -Chapas Necessarias <span class="num">${numChapas}</span>, `
         let restchapas = (areaChapas % 3.6).toFixed(2)
         if(restchapas > 0){
-        chapas.innerHTML += `e resta uma chapa de ${restchapas}m<sup>2</sup>`
+        chapas.innerHTML += `e resta uma chapa de <span class="num">${restchapas}m<sup>2</sup></span>`
         }
 
 
